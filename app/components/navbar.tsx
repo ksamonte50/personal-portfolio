d39992bbac617a3401dd1b882d81e9726a7e92dc
ksamonte50 <ksamonte50@gmail.com>
2024-12-16 17:07:03 -0800

    navbar structure, pictures, and css vars

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link"
+import Image from "next/image";
 
 export default function Navbar() {
     return (
-      <nav>
-        
+      <nav className="flex justify-between items-center mx-24 my-12">
+        <Image 
+            width={83}
+            height={83}
+            alt="KS Logo" 
+            src="/images/logo.png"
+            className="w-14 h-14"
+        />
+        <ul className="flex ">
+            <li><Link href="#">About</Link></li>
+            <li><Link href="#">Work</Link></li>
+            <li><Link href="#">Connect</Link></li>
+        </ul>
       </nav>
     );
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <Navbar />
   );
 }
